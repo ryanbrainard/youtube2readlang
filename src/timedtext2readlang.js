8a1ec13fae4e5c1b71a52d5e5813b131d7e2b126
Ryan Brainard <brainard@heroku.com>
2017-11-02 23:49:49 +0900

    Strip HTML if possible

--- src/timedtext2readlang.js
+++ src/timedtext2readlang.js
@@ -18,11 +18,19 @@ export function timedtext2readlangSync(result) {
   let wordCount = 0
 
   result.transcript.text.forEach((t) => {
-    const line = (t._ || '').trim()
+    const line = decodeHtml((t._ || '').trim())
     book.plainText += line + "\n\n"
     book.audioMap.push({t: parseFloat(t.$.start), w: wordCount})
     wordCount += countWords(line)
   })
 
   return book
+}
+
+function decodeHtml(str) {
+  if (typeof DOMParser !== 'function') {
+    return str
+  }
+
+  return new DOMParser().parseFromString('<!doctype html><body>' + str, 'text/html').body.textContent;
 }
